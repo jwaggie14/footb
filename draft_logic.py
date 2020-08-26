633bbb3e908632dcb8c4fa0662cdc655d08eb981
jwaggie14 <jlwhiting@outlook.com>
2020-08-26 17:59:22 -0500

    Update draft_logic.py

--- draft_logic.py
+++ draft_logic.py
@@ -1,110 +1,49 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import poisson
-import statsmodels.api as sm
-
-def predonadp(x):
-    X = x['adp']
-    X = sm.add_constant(X)
-    y = x['points']
-    mod = sm.OLS(y,X)
-    res = mod.fit()
-    pred = res.predict(X)
-    return pred
-
-def regonadp(x):
-    X = x['adp']
-    X = sm.add_constant(X)
-    y = x['points']
-    mod = sm.OLS(y,X)
-    res = mod.fit()
-    return res
 
 # df = pd.read_csv(r'C:\Users\james.whiting\OneDrive - Shell\Documents\ffa_customrankings2020-0.csv')
 
-pos_mask = df['position'].isin(['RB','QB','WR','TE','K','DL'])
-
-df = df.dropna(subset=['adp']).sort_values('adp')
-
-regressions = {pos:regonadp(df[df['position']==pos]) for pos in df.position.unique()}
-
-df['adp_points'] = df.dropna(subset=['adp']).groupby('position').apply(predonadp).reset_index(level=0, drop=True)
-df['blend'] = (df['points'] + df['adp_points']) / 2
-
-PROJ = 'blend'
-
-df = df[pos_mask]
-
-df = df.reset_index(drop=True)
-
-df['position'] = df['position'].replace('DL','D/ST')
 oc_mult = dict(zip(['QB','RB','WR','TE','K','D/ST'],[.3,.5,.5,.3,.15,.15]))
-df['bench_mult'] = df['position'].map(oc_mult)
 
-df['blacklist'] = False
+map_mult = lambda df: df['position'].map(oc_mult)
 
-df['bench_adj'] = df['position'].map(oc_mult)
+def next_picks(pick_order, mypick, thispick):
+    next_pick = pick_order.index(mypick,thispick) + 1
+    next_pick2 = pick_order.index(mypick,next_pick) + 1
+    return next_pick, next_pick2
 
-df = df.reset_index(drop=True)
+def p2f(x):
+    return float(x.strip('%'))/100
 
-teams = 12
-rounds = 16
-mypick = 6
-pick_order = (list(range(1,teams+1)) + list(range(teams,0,-1))) * (rounds//2)
+def expected_max(df):
+    df['pbest'] = df['probpicked'].cumprod().shift().fillna(1)
+    df['emaxw'] = df['pbest'] * df['%']
+    df['emax'] = df['points'] * df['emaxw']
+    return df
 
-mypick = 5
-this_pick = 35 # 1 indexed
-next_pick = pick_order.index(mypick,this_pick) + 1
-next_pick2 = pick_order.index(mypick,next_pick) + 1
+def adj_probs(players,probs, next_pick, next_pick2):
+    for i, pick in enumerate([next_pick,next_pick2]):
+        pmask = probs['pick'] == pick
+        df = players.merge(probs[pmask], how='left', left_on='espn_id',right_on='espnid')
+        df['%'] = df['%'].fillna(1)
+        df['%'] = np.where(df['picked'] | df['blacklist'], 0, df['%'])
+        df['probpicked'] = 1 - df['%']
+        df = df.groupby('position').apply(expected_max)
+        df['opp'] = df['position'].map(df.groupby('position')['emax'].sum())
+        players[f'oc_raw_{i}'] = (df['points'] - df['opp']) * df['probpicked'] * ~df['picked']
+        players[f'oc_adj_{i}'] = players[f'oc_raw_{i}'] * players['oc_adj']
+        players[f'pb_{i}'] = df['probpicked']
+    return players
 
-next_pick, next_pick2
+def blacklist_player(name,team,df):
+    name_match = df['player'] == name
+    team_match = df['team'] == team
+    mask = name_match & team_match
+    print("Blacklisting these players:")
+    print(df[mask][['player','team','position']])
+    df['blacklist'] = np.where(mask, True, df['blacklist'])
+    return df
 
-df['picked'] = df.index < this_pick
-
-positions = ['QB','RB','WR','TE','K','D/ST']
-
-roster_needs = ['RB','TE','K','D/ST']
-need_flex = False
-
-df['flex_position'] = df['position'].isin(['RB','WR','TE'])
-
-fdf = df[~df['picked'] & ~df['blacklist']].copy()
-fdf['need_pos'] = fdf['position'].isin(roster_needs) | (fdf['flex_position'] & need_flex)
-
-fdf['multiplier'] = ~fdf['need_pos'] * fdf['bench_mult'] + fdf['need_pos']
-
-fdf['prob_picked1'] = (fdf.apply(lambda x: poisson.cdf(next_pick, x['adp']), axis=1).fillna(0))
-fdf['prob_picked2'] = (fdf.apply(lambda x: poisson.cdf(next_pick2, x['adp']), axis=1).fillna(0))
-fdf['include1'] = fdf['prob_picked1'] > 0.01
-fdf['include2'] = fdf['prob_picked2'] > 0.01
-fdf['prob_avail1'] = (1 - fdf['prob_picked1']) * fdf['include1']
-fdf['prob_avail2'] = (1 - fdf['prob_picked2']) * fdf['include2']
-
-fdf['points_picked1'] = fdf['prob_picked1'] * fdf[PROJ]
-fdf['points_picked2'] = fdf['prob_picked2'] * fdf[PROJ]
-fdf['points_avail1'] = fdf['prob_avail1'] * fdf[PROJ]
-fdf['points_avail2'] = fdf['prob_avail2'] * fdf[PROJ]
-
-oc1cols = ['prob_picked1','prob_avail1','points_picked1','points_avail1']
-oc2cols = ['prob_picked2','prob_avail2','points_picked2','points_avail2']
-
-oc1 = fdf.groupby('position')[oc1cols].sum()
-oc1['points_picked1'] = oc1['points_picked1'] / oc1['prob_picked1']
-oc1['points_avail1'] = oc1['points_avail1'] / oc1['prob_avail1']
-
-oc1['oc'] = oc1['points_picked1'] - oc1['points_avail1']
-oc1['adj_oc'] = oc1['oc']
-
-oc1.round(2)
-
-oc2 = fdf.groupby('position')[oc2cols].sum()
-oc2['points_picked2'] = oc2['points_picked2'] / oc2['prob_picked2']
-oc2['points_avail2'] = oc2['points_avail2'] / oc2['prob_avail2']
-
-oc2['oc'] = oc2['points_picked2'] - oc2['points_avail2']
-
-oc2.round(2)
-
-
-
-
+def top_picks(df):
+    cols = ['player','team','position','points','adp','oc_adj_1','oc_adj_2']
+    print(df.sort_values('oc_adj_2').head(10)[cols])
