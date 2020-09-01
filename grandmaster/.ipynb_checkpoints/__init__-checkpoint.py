b324c507b2db2ec9fa7910247305364d3ae19259
jwaggie14 <jlwhiting@outlook.com>
2020-09-01 18:49:55 -0500

    fixed next pick calculation

--- grandmaster/.ipynb_checkpoints/__init__-checkpoint.py
+++ grandmaster/.ipynb_checkpoints/__init__-checkpoint.py
@@ -6,7 +6,6 @@ from .process_and_map_projections import *
 def tell_me_what_to_do(players,prob,draft):
     players = draft.filter_picks(players)
     players = draft.map_empty_positions(players)
-    np1, np2 = dl.next_picks(draft.pick_order,draft.myteam, draft.current_pick)
+    np1, np2 = dl.next_picks(draft.pick_order,draft.myteam, draft.current_pick+1)
     px = dl.adj_probs(players,prob,np1,np2)
-    dl.top_picks(px)
     return px
